042719bfa7e49de61c2d357f2f5eead4fca58771
Reginaldo MARINHO <marinhorce@gmail.com>
2024-08-10 16:53:50 +0200

    Siple model section developped

--- presentation/pages/4_Modelling.py
+++ presentation/pages/4_Modelling.py
@@ -17,20 +17,103 @@ st.set_page_config(page_title="CO2 Emissions", layout="wide")
 
 
 tab1, tab2 = st.tabs(["Regression", "Classification"])
+# -----------------------------------------
+# REGRESSION
+# -----------------------------------------
 with tab1:
     st.write("# To be developped")
-    
+
+# -----------------------------------------
+# CLASSIFICATION
+# -----------------------------------------
 with tab2:
+    st.write("# Classification Problem Definition")
+    with st.expander("Definition"):
+        st.markdown(
+            r"""
+ For this study we separate the CO2 emissions in 4 classes:
+ - `0` : Low < Q1
+ - `1` : Medium > Q1; <= Q2
+ - `3` : High > Q2; <= Q3
+ - `4` : Very High > Q3
+
+```python
+target  = pd.cut(
+    target,
+    bins = [0, 163, 202, 216, 572],
+    labels = [0, 1, 2, 3]
+)
+```
+
+ The objective of the model is then recognize each class correctly based on vehicle caracteristics.
+"""
+        )
+
+    simple_models_dict = {
+        "Logistic Regression": {
+            "model": "2-Logistic Regression.pkl",
+            "fig": "1-Logistic Regression Results.pkl",
+        },
+        "SVM": {"model": "2-SVM.pkl", "fig": "1-SVM Results.pkl"},
+        "KNN": {"model": "2-KNN.pkl", "fig": "1-KNN Results.pkl"},
+        "Decision Tree Classifier": {
+            "model": "2-Decision Tree Classifier.pkl",
+            "fig": "1-Decision Tree Results.pkl",
+        },
+        "RESULTS SUMMARY": {
+            "model": "2-Decision Tree Classifier.pkl",
+            "fig": "1-Results Summary Simple Classification.pkl",
+        },
+    }
+
     st.write("# Simple models")
-    with st.expander("# Simple models"):
-        st.write("to be developped")
+    with st.expander("Simple Models"):
+        options0 = [key for key in simple_models_dict]
+        option0 = st.selectbox("Select the model you want to check:", options0)
+
+        # Load result Graph
+        file_path = os.path.join(
+            modelling_dir, "imgs", simple_models_dict[option0]["fig"]
+        )
+        with open(file_path, "rb") as f:
+            fig = pickle.load(f)
+        st.plotly_chart(fig, use_container_width=True)
+        # Load Model
+        file_path = os.path.join(
+            modelling_dir, "models", simple_models_dict[option0]["model"]
+        )
+        with open(file_path, "rb") as f:
+            model = pickle.load(f)
+        # Load Data (only test is required as train already used to fit model)
+        file_path = os.path.join(modelling_dir, "data", "X_test.pkl")
+        with open(file_path, "rb") as f:
+            X_test = pickle.load(f)
+        file_path = os.path.join(modelling_dir, "data", "y_test.pkl")
+        with open(file_path, "rb") as f:
+            y_test = pickle.load(f)
+
+        # Display main results
+        col1, col2, col3 = st.columns(3)
+        y_pred = model.predict(X_test)
+        cm = pd.crosstab(
+            y_test,
+            y_pred,
+            rownames=["real"],
+            colnames=["predicted"],
+            # normalize='columns'
+        )
+
+        col1.write("### Confusion Matrix")
+        col1.write(cm)
+
+        col2.write("### Score")
+        col2.write(model.score(X_test, y_test))
 
     st.write("# Boosting and Bagging")
-    with st.expander("# Boosting and Bagging"):
+    with st.expander("Boosting and Bagging"):
         st.write("to be developped")
 
-
-    model_dict = {
+    grid_search_models_dict = {
         "K-nearest Neighbors": {
             "model": "Grid Search KNN.pkl",
             "fig": "1-Grid Search KNN.pkl",
@@ -45,23 +128,27 @@ with tab2:
         },
         "SVM": {"model": "Grid Search SVM.pkl", "fig": "1-Grid Search SVM.pkl"},
         "RESULTS SUMMARY": {
-            "model": "Grid Search SVM.pkl",
+            "model": "Grid Search Random Forest.pkl",
             "fig": "1-Results Summary Grid Search.pkl",
         },
     }
 
     st.write("# Grid Search")
     with st.expander("Grid Search Results"):
-        list_of_options = [key for key in model_dict]
+        list_of_options = [key for key in grid_search_models_dict]
         option = st.selectbox("Select the model you want to check:", list_of_options)
 
         # Load result Graph
-        file_path = os.path.join(modelling_dir, "imgs", model_dict[option]["fig"])
+        file_path = os.path.join(
+            modelling_dir, "imgs", grid_search_models_dict[option]["fig"]
+        )
         with open(file_path, "rb") as f:
             fig = pickle.load(f)
         st.plotly_chart(fig, use_container_width=True)
         # Load Model
-        file_path = os.path.join(modelling_dir, "models", model_dict[option]["model"])
+        file_path = os.path.join(
+            modelling_dir, "models", grid_search_models_dict[option]["model"]
+        )
         with open(file_path, "rb") as f:
             model = pickle.load(f)
         # Load Data (only test is required as train already used to fit model)
