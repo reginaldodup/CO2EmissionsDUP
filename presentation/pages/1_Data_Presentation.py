c3a445b8658a82a2ff857324f4cdba9ec7b4fee1
Reginaldo MARINHO <marinhorce@gmail.com>
2024-08-16 15:28:49 +0200

    Complete detailed description for quantitative variables and add corresponding section

--- presentation/pages/1_Data_Presentation.py
+++ presentation/pages/1_Data_Presentation.py
@@ -17,7 +17,7 @@ st.write("# Data Presentation")
 #     os.path.join(data_dir, "0-raw", "FRANCE", "carlab-annuaire-variable.xlsx"),
 #     sheet_name="EN",
 # ).fillna("-")
-# 
+#
 # st.table(df_info)
 df = pd.read_csv(os.path.join(data_dir, "0-raw", "FRANCE", "data_merged.csv"))
 
@@ -25,7 +25,7 @@ var_dict = {
     "CATEGORICAL": {
         "lib_mrq_utac": {
             "name": "lib_mrq_utac",
-            "description" : """
+            "description": """
 #### Brand
 
 The brand of the car manufacturer (the real brand of the car is indicated and
@@ -104,6 +104,8 @@ hybrid; `FE`: E85 super-ethanol; `GL`: Diesel plug-in electricity.
         "hybride": {
             "name": "hybride",
             "description": """
+#### Type of vehicle
+
 Information to identify hybrid vehicles.
 """,
         },
@@ -115,64 +117,183 @@ The type of gearbox (first letter) and the number of reports (the following
 number) i.e. 'M 6' means Manual Gearbox and 6 reports.
 """,
         },
-        "": {
-            "name": "",
-            "description": """""",
+        "champ_v9": {
+            "name": "champ_v9",
+            "description": """
+#### Certification of vehicle
+
+The champ_v9 corresponds to the certification of the vehicle. We could then
+separate them into certified and uncertified classes.
+""",
         },
-        "": {
-            "name": "",
-            "description": """""",
+        "date_maj": {
+            "name": "date_maj",
+            "description": """
+#### Date of the last update.
+""",
+        },
+        "Carrosserie": {
+            "name": "Carrosserie",
+            "description": """
+#### Car’s body type
+""",
+        },
+        "gamme": {
+            "name": "gamme",
+            "description": """
+#### Range
+
+The range of the car in term of quality (luxury car…).
+""",
         },
     },
     "QUANTITATIVE": {
-        "ADMIN POWER": {
-            "name": "",
-            "description": """""",
+        "co2": {
+            "name": "co2",
+            "description": """
+#### CO2 Emissions
+
+CO2 emission (in g/km) of the car. For the French dataset (2012-2015), the
+measure is according the NEDC norm.
+
+This is our `target variable`. In following steps of this project, we will define
+if we treat this as a regression problem or a classification problem (or both).
+When treating the problem as a classification this variable will be split in
+bins which represent a certain category of emissions (ex.: low, high, average,
+high and very high).
+""",
         },
-        "MAXIMUM POWER": {
-            "name": "",
-            "description": """""",
+        "puiss_admin_98": {
+            "name": "puiss_admin_98",
+            "description": """
+#### Administrative Power
+
+Administrative power is expressed in 'CV' (tax horsepower) and is used to
+estimate the tax amount on the car during registration of renewal of the
+vehicle registration document.
+
+In France, it exists two formulas to convert the motor power (kW) to
+administrative power (CV):
+
+- Approval from January 1, 2020:
+
+> $Admin Power (CV) = 1.34 + {\\left(1.8 \\times \\frac{Motor Power(kW)}{100}\\right)}^2 + {\\left(3.87 \\times \\frac{Motor Power(kW)}{100}\\right)}$
+
+- Approval before December 31, 2019:
+
+> $Admin Power (CV) = 1.34 + {\\frac{CO2 Emission (g/km)}{45}} + {\\frac{Motor Power(kW)}{40}} \\times 1.6$
+
+There is a correlation between the target variable CO2 Emissions and this
+variable. To be discussed during the next phase if this variable should be
+removed or not from the dataset.
+""",
         },
-        "CONSO URB": {
-            "name": "",
-            "description": """""",
+        "puiss_max": {
+            "name": "puiss_max",
+            "description": """
+#### Max power
+
+Maximum power of the motor expressed in KW.
+""",
         },
-        "CONSO EXURB": {
-            "name": "",
-            "description": """""",
+        "conso_urb": {
+            "name": "conso_urb",
+            "description": """
+#### Urban fuel consumption (in L/100km)
+
+This consumption corresponds to drive in an urban area with an acceleration up
+to 15 km/h, 30 km/h and 50 km/h. Including the most frequent stop, the urban
+fuel consumption is typically the higher consumption.
+""",
         },
-        "CONSO MIXTE": {
-            "name": "",
-            "description": """""",
+        "conso_exurb": {
+            "name": "conso_exurb",
+            "description": """
+#### Extra urban fuel consumption (in L/100km)
+
+This consumption corresponds to drive in an extra urban area with a drive on
+several speed levels up to 120 km/h/ It allows to optimize the driving and the
+fuel consumption of the car. Therefore, this consumption is generally the lower
+consumption.
+""",
         },
-        "CO TYPE 1": {
-            "name": "",
-            "description": """""",
+        "conso_mixte": {
+            "name": "conso_mixte",
+            "description": """
+#### Mixed fuel consumption (in L/100km)
+
+This consumption includes the drive in urban and extra urban area. Therefore,
+the fuel consumption is typically between the urban fuel consumption and the
+extra urban fuel consumption.
+""",
         },
-        "HC": {
-            "name": "",
-            "description": """""",
+        "hc": {
+            "name": "hc",
+            "description": """
+#### Unburned Hydrocarbons (HC) 
+
+Trial results measurement (g/km).
+""",
         },
-        "NOX": {
-            "name": "",
-            "description": """""",
+        "nox": {
+            "name": "nox",
+            "description": """
+#### NOx 
+
+Trial results measurement (g/km).
+""",
         },
-        "HCNOX": {},
+        "hcnox": {
+            "name": "hcnox",
+            "description": """
+#### $HC+NOx$ 
+
+Trial results measurement (g/km)
+""",
+        },
+        "ptcl": {
+            "name": "ptcl",
+            "description": """
+#### Particle 
+
+Trial results measurement (g/km)
+""",
+        },
+        "masse_ordma_min": {
+            "name": "masse_ordma_min",
+            "description": """
+#### The mass in minimum walking order (kg)
+
+It corresponds to the empty weight of the car with a gas bottle, 90% of the
+fluid necessary for the car to work and one driver (75 kg).
+""",
+        },
+        "masse_ordma_max": {
+            "name": "masse_ordma_max",
+            "description": """
+#### The mass in maximum walking order (kg)
+
+It corresponds to the weight that the vehicle must not exceed (include
+passengers and bags) 
+""",
+        },
     },
 }
 
 
-
-st.write("## Quantitative Values")
-with st.expander("Quantitative Values"):
-    selected_var = "typ_boite_nb_rapp"
-    st.write(var_dict['CATEGORICAL'][selected_var]['description'])
+st.write("## Categorical Variables")
+with st.expander("Categorical Variables"):
+    opts0 = [key for key in var_dict["CATEGORICAL"]]
+    opt0 = st.selectbox("Select the variable you want to check:", opts0)
+    selected_var = opt0
+    st.write(var_dict["CATEGORICAL"][selected_var]["description"])
 
     col1, col2 = st.columns(2)
-    col1.code(f"Missing values: {df[selected_var].isna().sum()}")
-    col2.code(f"Var type: {df[selected_var].dtype}")
+    col1.code(df[selected_var].describe())
+    # col2.code(f"Var type: {df[selected_var].dtype}")
+    col2.code(f"Missing values: {df[selected_var].isna().sum()}")
 
-    l = df[selected_var].unique()
+    l = df[selected_var].unique().tolist()
     st.write(f"#### Unique Values: `{len(l)}`")
     if len(l) > 600:
         col1, col2 = st.columns(2)
@@ -184,3 +305,16 @@ with st.expander("Quantitative Values"):
         st.code(df[selected_var].value_counts())
     else:
         st.code(list_to_cols(l, 5))
+
+st.write("## Quantitative Variables")
+with st.expander("Quantitative Values"):
+    opts1 = [key for key in var_dict["QUANTITATIVE"]]
+    opt1 = st.selectbox("Select the variable you want to check:", opts1)
+    selected_var = opt1
+    st.write(var_dict["QUANTITATIVE"][selected_var]["description"])
+
+    col1, col2 = st.columns(2)
+    col1.code(f"Missing values: {df[selected_var].isna().sum()}")
+    col2.code(f"Var type: {df[selected_var].dtype}")
+
+    st.code(df[selected_var].describe())
